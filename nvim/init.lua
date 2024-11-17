2697d957d03812cb04f3fc56af1e71daab89caee
Filippo Biondi <filibiondi2000@gmail.com>
2024-11-17 10:37:24 +0100

    fixed nvim-tree neogit conflict

--- nvim/init.lua
+++ nvim/init.lua
@@ -10,7 +10,20 @@ local g = vim.g
 g.loaded_netrw = 1
 g.loaded_netrwPlugin = 1
 
---require("nvim-tree").setup()
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
 
 opt.compatible = false
 
