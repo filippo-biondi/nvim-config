6023e2ea7534c96141626d1f623bbe4d2ebab4b6
Filippo Biondi <filibiondi2000@gmail.com>
2024-11-17 10:14:31 +0100

    readding nvim-tree

--- nvim/plugin/nvim-tree.lua
+++ nvim/plugin/nvim-tree.lua
@@ -3,17 +3,17 @@
 -- end
 -- vim.g.did_load_nvim_tree_plugin = true
 -- 
--- require("nvim-tree").setup({
---   sort = {
---     sorter = "case_sensitive",
---   },
---   view = {
---     width = 30,
---   },
---   renderer = {
---     group_empty = true,
---   },
---   filters = {
---     dotfiles = true,
---   },
--- })
+require("nvim-tree").setup({
+  sort = {
+    sorter = "case_sensitive",
+  },
+  view = {
+    width = 30,
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
+})
