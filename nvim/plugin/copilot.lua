864bad1fb535294c32a24243506772d70dc92d61
Filippo Biondi <filibiondi2000@gmail.com>
2025-03-16 10:32:10 +0100

    Changed copilot complete keybindings.

--- nvim/plugin/copilot.lua
+++ nvim/plugin/copilot.lua
@@ -2,11 +2,11 @@ vim.keymap.set('i', '<C-Down>', 'copilot#Accept("<C-Down>")', {
   expr = true,
   replace_keycodes = false
 })
-vim.keymap.set('i', '<C-Right>', 'copilot#AcceptLine("<C-Right>")', {
+vim.keymap.set('i', '<C-End>', 'copilot#AcceptLine("<C-End>")', {
   expr = true,
   replace_keycodes = false
 })
-vim.keymap.set('i', '<Right>', 'copilot#AcceptWord("<Right>")', {
+vim.keymap.set('i', '<C-Right>', 'copilot#AcceptWord("<C-Right>")', {
   expr = true,
   replace_keycodes = false
 })
