1c74d2f7e3e0c3edbac721845027bcec75c02436
Filippo Biondi <filibiondi2000@gmail.com>
2025-03-17 16:54:57 +0100

    Changed from C-Right to S-Right for accepting copilot suggestion.

--- nvim/plugin/copilot.lua
+++ nvim/plugin/copilot.lua
@@ -1,12 +1,12 @@
-vim.keymap.set('i', '<C-Down>', 'copilot#Accept("<C-Down>")', {
+vim.keymap.set('i', '<S-Down>', 'copilot#Accept("<S-Down>")', {
   expr = true,
   replace_keycodes = false
 })
-vim.keymap.set('i', '<C-End>', 'copilot#AcceptLine("<C-End>")', {
+vim.keymap.set('i', '<S-End>', 'copilot#AcceptLine("<S-End>")', {
   expr = true,
   replace_keycodes = false
 })
-vim.keymap.set('i', '<C-Right>', 'copilot#AcceptWord("<C-Right>")', {
+vim.keymap.set('i', '<S-Right>', 'copilot#AcceptWord("<S-Right>")', {
   expr = true,
   replace_keycodes = false
 })
