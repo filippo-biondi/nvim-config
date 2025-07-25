652da209f5518af2cf35acc4eb3234dffe58fffc
Filippo Biondi <filibiondi2000@gmail.com>
2025-07-25 19:08:57 +0200

    Added binding for autoformatter and remove formatting on save

--- nvim/plugin/conform.lua
+++ nvim/plugin/conform.lua
@@ -5,9 +5,13 @@ require("conform").setup({
   },
 })
 
-vim.api.nvim_create_autocmd("BufWritePre", {
-  pattern = "*",
-  callback = function(args)
-    require("conform").format({ bufnr = args.buf })
-  end,
-})
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   pattern = "*",
+--   callback = function(args)
+--     require("conform").format({ bufnr = args.buf })
+--   end,
+-- })
+
+vim.keymap.set("n", "<leader>cf", function()
+  require("conform").format({ bufnr = vim.api.nvim_get_current_buf() })
+end, { desc = "Format current buffer" })
