9744f3cb7759c5e13823a011b7b8b32b4db48a16
Filippo Biondi <filibiondi2000@gmail.com>
2025-07-25 19:02:08 +0200

    Improved smartdelete

--- nvim/plugin/smartdelete.lua
+++ nvim/plugin/smartdelete.lua
@@ -4,13 +4,14 @@ local function smart_delete(key)
 	return (line:match("^%s*$") and '"_' or "") .. key -- If the line is empty or contains only whitespace, use the black hole register
 end
 
-local keys = { "d", "dd", "x", "c", "C", "X" } -- Define a list of keys to apply the smart delete functionality
+local keys = { "d", "dd", "c", "C"} -- Define a list of keys to apply the smart delete functionality
 
 -- Set keymaps for both normal and visual modes
 for _, key in pairs(keys) do
-	vim.keymap.set({ "n", "v" }, key, function()
+	vim.keymap.set("n", key, function()
 		return smart_delete(key)
 	end, { noremap = true, expr = true, desc = "Smart delete" })
 end
 
 vim.keymap.set("n", "x", '"_x', { noremap = true, desc = "Smart delete char" }) -- Override 'x' to use the black hole register
+vim.keymap.set("n", "X", '"_dd', { noremap = true, desc = "Smart delete line" })
